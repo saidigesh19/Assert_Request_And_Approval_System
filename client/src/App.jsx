5090caca126f4b93e5ae7cc111ae08728822b00a
Mohana Prawin Elamprabhu <mohana.elamprabhu@ispace.com>
2025-12-30 16:32:42 +0530

    login page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css'
 import AdminDashboard from './components/AdminDashboard'
+import LoginPage from './components/login'
 function App() {
 
 return (
     <div>
-      <AdminDashboard/>
+      <LoginPage/>
     </div>
   )
 }
