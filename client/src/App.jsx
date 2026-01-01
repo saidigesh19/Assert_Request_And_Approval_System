42373b1f9425b06f4a059b1c1cab99afb44e1f5c
Sravanthi Alluri <sravanthi.alluri@ispace.com>
2026-01-01 14:36:29 +0530

    Added my request page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AssetRequest from './components/assetRequest';
 import LoginPage from './components/login';
 import Register from './components/register';
+import MyRequest from './components/myRequests';
+
 function App() {
 
 return (
@@ -11,6 +13,7 @@ return (
         <Route path="/" element={<LoginPage/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="/assetrequest" element={<AssetRequest/>} />
+        <Route path="/myrequest" element={<MyRequest/>}/>
       </Routes>
     </BrowserRouter>
   )
