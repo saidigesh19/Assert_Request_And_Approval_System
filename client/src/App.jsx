f488634aa60d91a2b73dc022e548d95732161b8d
Sravanthi Alluri <sravanthi.alluri@ispace.com>
2026-01-02 09:15:52 +0530

    Added routing to dashboard and myrequest pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,6 @@ import AssetRequest from './components/Employee/assetRequest';
 import LoginPage from './components/Auth/login';
 import Register from './components/Auth/register';
 import MyRequest from './components/Employee/myRequests';
-import AdminDashboard from './components/Admin/adminDashboard';
 function App() {
 
 return (
@@ -13,8 +12,8 @@ return (
         <Route path="/" element={<LoginPage/>} />
         <Route path="/register" element={<Register/>} />
         <Route path="/assetrequest" element={<AssetRequest/>} />
-        <Route path="/admindashboard" element={<AdminDashboard/>} />
         <Route path="/myrequest" element={<MyRequest/>} />
+        <Route path="/dashboard" element={<AdminDashboard/>} />
       </Routes>
     </BrowserRouter>
   )
---
+++ client/src/components/Employee/myRequests.jsx
@@ -0,0 +1,115 @@
+import { FaSearch } from "react-icons/fa";
+
+const MyRequest = () => {
+  const tableData = [
+    {
+      assetType: "Mouse",
+      reason: "My mouse is not working",
+      status: "Pending",
+      requestedOn: "12/03/2020",
+    },
+    {
+      assetType: "Laptop",
+      reason: "Upgrade to a faster model",
+      status: "Rejected",
+      requestedOn: "12/03/2020",
+    },
+    {
+      assetType: "keyboard",
+      reason: "Broken keys on current keyboard",
+      status: "Approved",
+      requestedOn: "12/03/2020",
+    },
+    {
+      assetType: "Mouse",
+      reason: "My mouse is not working",
+      status: "Pending",
+      requestedOn: "12/03/2020",
+    },
+  ];
+  return (
+    <div className="mx-auto w-full h-screen bg-gray-50">
+      <div className="w-3xl mx-auto flex flex-col">
+        <h1 className="font-bold text-2xl text-left">My Requests</h1>
+      <div className="border text-left bg-white border-gray-300 rounded mt-5">
+        <div className="m-2 flex flex-row justify-between gap-20 border border-gray-50 rounded text-left bg-gray-100">  
+          <div className="w-full h-12 p-3 items-center border rounded-sm border-gray-200 text-sm bg-white">
+          <label>Status :</label>  
+          <select>
+            <option>All</option>
+            <option>Pending</option>
+            <option>Approved</option>
+            <option>Rejected</option>
+          </select>
+          </div>
+          <div className="relative w-full max-w-sm flex justify-end">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-full h-10 px-2 mt-1 text-sm border border-gray-200 rounded-md focus:ring-gray-500 bg-white"
+            />
+            <FaSearch className="absolute m-4 text-gray-500" />
+          </div>
+        </div>
+
+        <div className="relative overflow-x-auto shadow-xs rounded-base">
+          <table className="w-full text-sm text-left rtl:text-right text-body">
+            <thead className="text-sm bg-gray-100 border-t border-b border-gray-200">
+              <tr>
+                <th scope="col" className="px-6 py-3 font-medium">
+                  Asset type
+                </th>
+                <th scope="col" className="px-6 py-3 font-medium">
+                  Reason
+                </th>
+                <th scope="col" className="px-6 py-3 font-medium">
+                  Status
+                </th>
+                <th scope="col" className="px-6 py-3 font-medium">
+                  Requested On
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((item, index) => {
+                let statusColor = "";
+                if (item.status.toLowerCase() === "approved") {
+                  statusColor = "bg-green-600 text-white";
+                } else if (item.status.toLowerCase() === "rejected") {
+                  statusColor = "bg-red-600 text-white";
+                } else if (item.status.toLowerCase() === "pending") {
+                  statusColor = "bg-yellow-600 text-white";
+                }
+                return (
+                  <tr
+                    key={index}
+                    className="bg-neutral-primary border-b border-gray-200"
+                  >
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-heading whitespace-nowrap"
+                    >
+                      {item.assetType}
+                    </th>
+                    <td className="px-6 py-4">{item.reason}</td>
+                    <td className="px-6 py-4">
+                      <button
+                        className={`border px-2 py-1 rounded ${statusColor}`}
+                      >
+                        {item.status}
+                      </button>
+                    </td>
+                    <td className="px-6 py-4">{item.requestedOn}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default MyRequest;
