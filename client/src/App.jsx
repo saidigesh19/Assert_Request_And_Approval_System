0ea986a58cb34a4f62c3ccbf76bb339eb945899b
Sravanthi Alluri <sravanthi.alluri@ispace.com>
2026-01-09 11:51:56 +0530

    Added protected routes to admin and employee

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import LoginPage from './components/Auth/login';
 import Register from './components/Auth/register';
 import MyRequest from './components/Employee/myRequests';
 import AdminDashboard from './components/Admin/adminDashboard';
+import ProtectedRoute from './components/protectedRoutes';
 function App() {
 
   return (
@@ -12,9 +13,25 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage/>} />
         <Route path="/register" element={<Register/>} />
-        <Route path="/assetrequest" element={<AssetRequest/>} />
-        <Route path="/myrequest" element={<MyRequest/>} />
-        <Route path="/dashboard" element={<AdminDashboard/>} />
+        
+         {/* employee Routes */}
+        <Route path="/assetrequest" element={
+          <ProtectedRoute allowedRoles={["Employee"]}>
+          <AssetRequest/>
+          </ProtectedRoute>
+          } />
+        <Route path="/myrequest" element={
+          <ProtectedRoute allowedRoles={["Employee"]}>
+          <MyRequest/>
+          </ProtectedRoute>
+          } />
+
+          {/* admin routes */}
+        <Route path="/dashboard" element={
+          <ProtectedRoute allowedRoles={["Admin"]}>
+          <AdminDashboard/>
+          </ProtectedRoute>
+          } />
       </Routes>
     </BrowserRouter>
   )
